d75548a7d544458d09d9afbec793d9d64b1ea52d
carolusquintus <carloschars@gmail.com>
2014-11-21 19:10:51 -0600

    Packages created to define modules per classes.

--- src/main/groovy/com/carm/oso/rest/service/UserController.groovy
+++ src/main/groovy/com/carm/oso/rest/service/UserController.groovy
@@ -1,16 +1,14 @@
-package com.carm.oso
+package com.carm.oso.rest.service
 
-import org.springframework.dao.DataIntegrityViolationException
+import com.carm.oso.domain.User
 
-import javax.ws.rs.DELETE
+//import org.springframework.dao.DataIntegrityViolationException
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
-import javax.ws.rs.core.Response
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON
-import static javax.ws.rs.core.MediaType.TEXT_HTML
 
 /**
  * Date: Nov 01, 2014
@@ -23,7 +21,10 @@ class UserController {
     @GET
     @Produces(APPLICATION_JSON)
     User userByName(@QueryParam("name") String name) {
-        User u = User.findByName(name)
+        //User u = User.findByName(name)
+        User u = new User(id: 1, name: "", firstSurname: "", secondSurname: "",
+                        email: "", phone: "", companyId: "", password: "",
+                        emailValidated: false, accountLocked: true, passwordExpired: false, organizationsUrl: "")
         return u
     }
 
@@ -31,10 +32,17 @@ class UserController {
     @Path("/all")
     @Produces(APPLICATION_JSON)
     List<User> allUsers() {
-        List<User> users = User.findAll()
+        //List<User> users = User.findAll()
+        List<User> users = [new User(id: 1, name: "", firstSurname: "", secondSurname: "",
+                            email: "", phone: "", companyId: "", password: "",
+                            emailValidated: false, accountLocked: true, passwordExpired: false, organizationsUrl: ""),
+                            new User(id: 1, name: "", firstSurname: "", secondSurname: "",
+                            email: "", phone: "", companyId: "", password: "",
+                            emailValidated: false, accountLocked: true, passwordExpired: false, organizationsUrl: "")]
         return users
     }
 
+    /*
     @DELETE
     @Path("/delete")
     @Produces(TEXT_HTML)
@@ -46,7 +54,7 @@ class UserController {
         } catch (DataIntegrityViolationException e) {
             return Response.status(404).entity("Could not delete person ${user.id}").build()
         }
-    }
+    }*/
 
     /*
     //@RequestMapping(value = "/user/add", method = RequestMethod.POST)
